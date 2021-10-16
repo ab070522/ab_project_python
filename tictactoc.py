00bbbd32f4df4ed13e0cd5c83b2fbfd415ede029
ab070522 <88918803+ab070522@users.noreply.github.com>
2021-10-16 12:28:27 +0900

    Add files via upload

--- tictactoc.py
+++ tictactoc.py
@@ -1,4 +1,3 @@
-
 import random
 def make_map():
 
@@ -12,26 +11,15 @@ def put_avatar(tic_map):
         if tic_map[pos - 1] == " ":
             tic_map[pos - 1] = "x"
 
-
             return tic_map
 
 def put_AI_avatar(tic_map):
     while True:
-        AI = random.randrange(1, 11)
+        AI = random.randrange(1, 10)
         if tic_map[AI - 1] == " ":
             tic_map[AI - 1] = "o"
 
             return tic_map
-
-def bingo(num):
-
-    tic_map[3] == num and tic_map[4] == num and tic_map[5] == num
-    tic_map[6] == num and tic_map[7] == num and tic_map[8] == num
-    tic_map[0] == num and tic_map[4] == num and tic_map[8] == num
-    tic_map[2] == num and tic_map[4] == num and tic_map[6] == num
-    tic_map[0] == num and tic_map[3] == num and tic_map[6] == num
-    tic_map[1] == num and tic_map[4] == num and tic_map[7] == num
-    tic_map[2] == num and tic_map[5] == num and tic_map[8] == num
 
 def print_map(tic_map):
     print("ㅣ " + tic_map[0] + " ㅣ " + tic_map[1] + " ㅣ " + tic_map[2] + " ㅣ ")
@@ -41,6 +29,29 @@ def print_map(tic_map):
     print("ㅣ " + tic_map[6] + " ㅣ " + tic_map[7] + " ㅣ " + tic_map[8] + " ㅣ ")
     print("---------------")
 
+def find_bingo(num):
+    score = 0
+    if [3] == num and tic_map[4] == num and tic_map[5] == num:
+        if num == 'x':
+            score = 1
+        else:
+            score = -1
+    elif [6] == num and tic_map[7] == num and tic_map[8] == num:
+        bingo = 1
+    elif [0] == num and tic_map[4] == num and tic_map[8] == num:
+        bingo = 1
+    elif [2] == num and tic_map[4] == num and tic_map[6] == num:
+        bingo = 1
+    elif [0] == num and tic_map[3] == num and tic_map[6] == num:
+        bingo = 1
+    elif [1] == num and tic_map[4] == num and tic_map[7] == num:
+        bingo = 1
+    elif [2] == num and tic_map[5] == num and tic_map[8] == num:
+        bingo = 1
+    else:
+        bingo = 0
+    return bingo
+
 #맵 만들기
 tic_map = make_map()
 
@@ -49,20 +60,8 @@ bingo = 0
 
 while bingo == 0:
     tic_map = put_avatar(tic_map)
+    bingo = find_bingo("x")
     tic_map = put_AI_avatar(tic_map)
     print_map(tic_map)
-    if tic_map[0] == "x" and tic_map[1] == "x" and tic_map[2] == "x"
-    def bingo(num)
-    tic_map[3] == "x" and tic_map[4] == "x" and tic_map[5] == "x"
-    tic_map[6] == "x" and tic_map[7] == "x" and tic_map[8] == "x"
-    tic_map[0] == "x" and tic_map[4] == "x" and tic_map[8] == "x"
-    tic_map[2] == "x" and tic_map[4] == "x" and tic_map[6] == "x"
-    tic_map[0] == "x" and tic_map[3] == "x" and tic_map[6] == "x"
-    tic_map[1] == "x" and tic_map[4] == "x" and tic_map[7] == "x"
-    tic_map[2] == "x" and tic_map[5] == "x" and tic_map[8] == "x":
-        bingo = 1
-        print("player win!!")
-
-        print("AI win!!")
-
+    bingo = find_bingo("o")
 
